0182014bf394ee7bf4e093008615d418704b585d
ZuriGa <zurisadailopezmartinez@gmail.com>
2023-11-03 16:00:16 -0700

    fourth test passed, updated code to get the remove toppings correctly

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ## Describe: Pizza()
 Test 1: "It should return a Pizza object with two properties for toppings and size"\
 Code: const myPizza = new Pizza("medium", ["anchovies", "pineapple"]);\
-Expected Output: Pizza { toppings: ["anchovies", "pineapple"], size: "medium" }
+Expected Output: Pizza { size: "medium", toppings: ["anchovies", "pineapple"] }
 
 Test 2: "It should calculate the cost of the pizza based on selected toppings and size"\
 Code: calculateCost();\
@@ -10,11 +10,11 @@ Output: 10, 12, 18
 
 Test 3: "It should allow toppings to be added to the pizza"\
 Code: addTopping();\
-Output: 18.00
+Output: 13
 
 Test 4: "It should allow toppings to be remove from pizza"\
 Code: removeTopping();\
-Output:
+Output: 12
 
 Test 5: "It should calculate the cost of the pizza after adding or removing toppings"\
 Code: updateCost();\
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
-function Pizza(size, addToppings, removeToppings) {
+function Pizza(size, initToppings) {
     this.size = size;
-    this.addToppings = addToppings;
-    this.removeToppings = removeToppings;
+    this.toppings = initToppings || ["cheese", "tomato sauce"];
+
+    this.addTopping = function (topping) {
+        this.toppings.push(topping);
+    };
+
+    this.removeTopping = function (topping) {
+        const index = this.toppings.indexOf(topping);
+        if (index !== -1) {
+            this.toppings.splice(index, 1);
+        }
+
+    };
 }
+
+
+
 
 Pizza.prototype.calculateCost = function () {
     let basePrice = 10; //Default based price
@@ -15,10 +29,14 @@ Pizza.prototype.calculateCost = function () {
         basePrice *= 1.8;
     }
 
-    const addedToppingsCost = this.addToppings.length * 1;
+    const addedToppingsCost = this.toppings.length - 2;
 
     const totalCost = basePrice + addedToppingsCost;
 
     return totalCost.toFixed(2);
 
 };
+
+const pizza = new Pizza("medium", ["cheese", "mushroom", "pepperoni"]);
+pizza.removeTopping("mushroom"); // Remove a topping
+console.log(pizza.calculateCost()); // Output: "13.00"
